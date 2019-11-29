f222069490974f60b800fa5ca773760ab87d44ee
Esteban Salas <esalas21@uan.edu.co>
2019-11-28 22:03:14 -0500

    Pruebas unitarias del parcial

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+public class Calculadora {
+
+    public static int suma(int a , int b){
+        return a+b;
+    }
+    
+    public static int resta(int a , int b){
+        return a-b;
+    }
+    
+    public static int multiplicar(int a , int b){
+        return a*b;
+    }
+    
+    public static double div(double a , double b){
+        return a/b;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Calculadora/test/PruebasCalculadora/testCalculadora.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PruebasCalculadora;
+
+import calculadora.Calculadora;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Esteban
+ */
+public class testCalculadora {
+    
+    public testCalculadora() {
+    }
+    
+    @Test
+    public void testSuma(){
+        int resultado = Calculadora.suma(5,6);
+        int espearado = 11;
+        assertEquals(espearado, resultado);
+    }
+    @Test
+    public void testResta(){
+        int resultado = Calculadora.resta(5,6);
+        int espearado = -1;
+        assertEquals(espearado, resultado);
+    }
+    @Test
+    public void testMultiplicar(){
+        int resultado = Calculadora.multiplicar(5,6);
+        int espearado = 30;
+        assertEquals(espearado, resultado);
+    }
+    @Test
+    public void testDiv(){
+        double resultado = Calculadora.div(6.0 ,5.0);
+        double espearado = 1.2;
+        assertEquals(espearado, resultado, 0.00);
+    }
+}
